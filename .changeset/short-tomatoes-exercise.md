8ab47e1a2a931d86ad08be01502dec034ab295cf
Nathan Kanigsberg <nathan@kanigsberg.com>
2022-06-09 10:29:23 -0400

    publish attempt

---
+++ .changeset/short-tomatoes-exercise.md
@@ -0,0 +1,9 @@
+---
+"@nathan-test/docs": major
+"@nathan-test/core": major
+"@nathan-test/tsconfig": major
+"@nathan-test/utils": major
+"eslint-config-nathan-test": major
+---
+
+This is a summary test
--- apps/docs/.storybook/main.js
+++ apps/docs/.storybook/main.js
@@ -14,10 +14,10 @@ module.exports = {
       resolve: {
         alias: [
           {
-            find: "@acme/core",
+            find: "@nathan-test/core",
             replacement: path.resolve(
               __dirname,
-              "../../../packages/acme-core/"
+              "../../../packages/nathan-test-core/"
             ),
           },
         ],
